b4811efcc10e48869a3a3aa189ca15b8bc8f02b1
root <t_rei_512@yahoo.co.jp>
2023-06-10 09:26:02 +0900

    build sucssessful

--- src/turtlesim_operation/setup.py
+++ src/turtlesim_operation/setup.py
@@ -6,11 +6,15 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    py_modules=[
+	'turtlesim_operation.turtlesim_operation', 
+	'turtlesim_operation.turtlesim_ope_widget',
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+    ], 
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
@@ -20,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+		'controller= turtlesim_operation.turtlesim_operation'
         ],
     },
 )
