85ad9d99f20467224d4a02f78cae7f916bc47018
root <t_rei_512@yahoo.co.jp>
2023-06-14 02:36:31 +0900

    modifing to use custom_messages/Can.msg

--- src/can_communicate_node_gui/src/main_script.cpp
+++ src/can_communicate_node_gui/src/main_script.cpp
@@ -4,20 +4,21 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+//#include "std_msgs/msg/string.hpp"
+#include "custom_messages/msg/canmsg.hpp"	//custom message
 
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class rosNode : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
+    rosNode()
+    : Node("rosNode"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<custom_messages::msg::CanMsg>("/can_rx", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -25,13 +26,14 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+      auto can = custom_messages::msg::CanMsg();
+      can.id = 32;
+      can.dlc = count_++;
+      RCLCPP_INFO(this->get_logger(), "Publishing: 'id:%d dlc:%d'", can.id, can.dlc);
+      publisher_->publish(can);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<custom_messages::msg::CanMsg>::SharedPtr publisher_;
     size_t count_;
 };
 
