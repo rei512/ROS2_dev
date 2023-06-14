7c5760bcf430c341e8909dd7484bb1a593ddd081
root <t_rei_512@yahoo.co.jp>
2023-06-15 08:44:14 +0900

    input

--- src/can_communicate_node_gui/src/main_script.cpp
+++ src/can_communicate_node_gui/src/main_script.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 //#include "std_msgs/msg/string.hpp"
@@ -15,32 +16,60 @@ using namespace std::chrono_literals;
 class rosNode : public rclcpp::Node
 {
   public:
-    rosNode()
-    : Node("rosNode"), count_(0)
-    {
-      publisher_ = this->create_publisher<custom_messages::msg::CanMsg>("/can_rx", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&rosNode::timer_callback, this));
-    }
+	rosNode()
+	: Node("rosNode"), count_(0)
+	{
+	  publisher_ = this->create_publisher<custom_messages::msg::CanMsg>("/can_rx", 10);
+	  //RCLCPP_INFO(this->get_logger(), "Publishing: 'id:%d dlc:%d'", can.id, can.dlc);
+	  //timer_ = this->create_wall_timer(
+	  //500ms, std::bind(&rosNode::timer_callback, this));
+	}
+
+	void loop() {
+		while(1) {
+			std::string input;
+			std::cout << "> ";
+			std::cin >>input;
+			if(input[2] != '!') {
+				if(input[2] == '\n') {
+					std::cout << "Empty!" << std::endl;
+				}
+				publish();
+				RCLCPP_INFO(this->get_logger(), "Publishing: 'id:%d dlc:%d'", can.id, can.dlc);
+			}
+			if(!input.compare(1, 4, "exit")) {
+				std::cout << "exit" << std::endl;
+				return;
+			}
+		}
+	}
 
   private:
-    void timer_callback()
-    {
-      auto can = custom_messages::msg::CanMsg();
-      can.id = 32;
-      can.dlc = count_++;
-      RCLCPP_INFO(this->get_logger(), "Publishing: 'id:%d dlc:%d'", can.id, can.dlc);
-      publisher_->publish(can);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<custom_messages::msg::CanMsg>::SharedPtr publisher_;
-    size_t count_;
+	//void timer_callback()
+	//{
+	//  auto can = custom_messages::msg::CanMsg();
+	//  can.id = 32;
+	//  can.dlc = count_++;
+	//  RCLCPP_INFO(this->get_logger(), "Publishing: 'id:%d dlc:%d'", can.id, can.dlc);
+	//  publisher_->publish(can);
+	//}
+	//rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<custom_messages::msg::CanMsg>::SharedPtr publisher_;
+	size_t count_;
+	custom_messages::msg::CanMsg can;
+
+	void publish() {
+		publisher_->publish(can);
+	}
+
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<rosNode>());
+  rosNode can;
+  //rclcpp::spin(std::make_shared<rosNode>());
+  can.loop();
   rclcpp::shutdown();
   return 0;
 }
